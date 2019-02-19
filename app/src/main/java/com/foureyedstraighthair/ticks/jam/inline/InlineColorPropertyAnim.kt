9eadd6280e828420ecf097afc0c656e7f18755eb
foureyedstraighthair <hayashibambi@gmail.com>
2019-02-19 13:50:02 +0900

    ulu

--- app/src/main/java/com/foureyedstraighthair/ticks/jam/inline/InlineColorPropertyAnim.kt
+++
@@ -1,9 +0,0 @@
-package com.foureyedstraighthair.ticks.jam.inline
-
-import android.content.Context
-import android.util.AttributeSet
-
-class InlineColorPropertyAnim(
-    context: Context, attributeSet: AttributeSet)
-    : InlinePropertyAnim<InlineColorProperty>(
-    context, attributeSet, InlineColorProperty::class.java)
